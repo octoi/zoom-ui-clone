0f14729bcf9a6ef2f3da5b497614b92fca4fec9b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-28 18:15:30 +0530

    cleanup, colors & stuff

---
+++ app/lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color appBg = Colors.black;
+const Color appRed = Color(0xFFDE2828);
+const Color appGreen = Color(0xFF23D959);
+const Color appWhite = Colors.white;
