a0b21d2bddb2a8094e89b9d07b5b334def6b06a2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-29 03:45:03 +0530

    rick : )

--- app/lib/widgets/meet.dart
+++ app/lib/widgets/meet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const String rickImage =
+    'https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png';
+
 class AppMeet extends StatelessWidget {
   const AppMeet({Key? key}) : super(key: key);
 
@@ -11,41 +14,55 @@ class AppMeet extends StatelessWidget {
         children: [
           Flexible(
             child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                    margin: EdgeInsets.all(5.0),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                    margin: EdgeInsets.all(5.0),
-                  ),
-                ),
+              children: const [
+                MeetVideo(image: rickImage, name: 'Rick Astley'),
+                MeetVideo(image: rickImage, name: 'Rick Astley'),
               ],
             ),
           ),
           Flexible(
             child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                    margin: EdgeInsets.all(5.0),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                    margin: EdgeInsets.all(5.0),
-                  ),
-                ),
+              children: const [
+                MeetVideo(image: rickImage, name: 'Rick Astley'),
+                MeetVideo(image: rickImage, name: 'Rick Astley'),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class MeetVideo extends StatelessWidget {
+  final String image;
+  final String name;
+
+  const MeetVideo({
+    Key? key,
+    required this.image,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(image),
+            fit: BoxFit.cover,
+          ),
+        ),
+        margin: EdgeInsets.all(2.0),
+        child: Stack(
+          children: [
+            Container(
+              child: Text(name),
+            )
+          ],
+        ),
       ),
     );
   }
